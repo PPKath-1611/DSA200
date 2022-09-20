45f626755a68c1660ab00247822f26662df08646
PPKath-1611 <68817942+PPKath-1611@users.noreply.github.com>
2022-09-20 04:23:42 -0700

    Time: 126 ms (87.35%), Space: 47.3 MB (48.05%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end(), greater<int>());
-        return nums[k-1];
+        priority_queue<int , vector<int>, greater<int>> minh;
+        for(int i: nums)
+        {
+            minh.push(i);
+            if(minh.size()>k)
+              minh.pop();
+        }
+        return minh.top();
     }
 };
